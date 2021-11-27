1a0b18d2e6a667c6cec7c30e89f0bf850c893267
Umesh Patidar <50135590+umeshdangrecha@users.noreply.github.com>
2021-11-27 14:21:51 +0530

    go back and forward

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
 
 const MealDetailScreen = (props) => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>MealDetailScreen</Text>
+      <Button
+        title="Back"
+        onPress={() => {
+          props.navigation.goBack();
+        }}
+      />
     </View>
   );
 };
