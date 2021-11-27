97e9276ef1aa4169e34952540eb68fdd4530239b
Umesh Patidar <50135590+umeshdangrecha@users.noreply.github.com>
2021-11-27 18:00:20 +0530

    setting dynamic data

--- screens/CategoryMeals.js
+++ screens/CategoryMeals.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Platform } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
+import Colors from "../constants/Colors";
 
 const CategoryMeals = (props) => {
   const catId = props.navigation.getParam("categoryId");
@@ -12,6 +13,19 @@ const CategoryMeals = (props) => {
   );
 };
 
+CategoryMeals.navigationOptions = (navigationData) => {
+  // console.log("CategoryMeals", navigationData);
+  const catId = navigationData.navigation.getParam("categoryId");
+  const selectedData = CATEGORIES.find((item) => item.id === catId);
+
+  return {
+    headerTitle: selectedData.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+  };
+};
 const styles = StyleSheet.create({});
 
 export default CategoryMeals;
